cb5c6bc8815db5e682096032ca02995c141a5514
AkitidaKamaki <rerategaming@gmail.com>
2022-06-21 00:13:32 +0200

    get all stations

--- api/routes/api.php
+++ api/routes/api.php
@@ -24,7 +24,7 @@ Route::controller(App\Http\Controllers\UserController::class)->group(function(){
 //Route::middleware(['auth:sanctum', 'ability:weather,website'])->group( function () {
     //protected routes
 Route::get('weather/recent/{station_name}','App\Htrp\ontrollers\WeatherdataController@recent');
-    Route::get('weather', 'App\Http\Controllers\WeatherdataController@index');
+    //Route::get('weather', 'App\Http\Controllers\WeatherdataController@index');
     Route::get('weather/{id}', 'App\Http\Controllers\WeatherdataController@show');
     Route::get('weather/station/{station_name}', 'App\Http\Controllers\WeatherdataController@search');
     Route::get('weather/station', 'App\Http\Controllers\WeatherdataController@search2');
@@ -32,12 +32,15 @@ Route::get('weather/recent/{station_name}','App\Htrp\ontrollers\WeatherdataContr
     Route::get('weather/{date}/{time}', 'App\Http\Controllers\WeatherdataController@searchTime');
     Route::get('weather/station/{station_name}/{date}', 'App\Http\Controllers\WeatherdataController@searchStationDate');
     Route::get('weather/station/{station_name}/{date}/{time}', 'App\Http\Controllers\WeatherdataController@searchStationDateTime');
-    Route::post('weather/getData', 'App\Http\Controllers\WeatherdataController@getData');
-    Route::post('weather/getDataV2', 'App\Http\Controllers\WeatherdataController@getAdvData');
+    //Route::post('weather/getData', 'App\Http\Controllers\WeatherdataController@getData');
+    //Route::post('weather/getDataV2', 'App\Http\Controllers\WeatherdataController@getAdvData');
 //});
 
 Route::middleware(['auth:sanctum', 'ability:userinfo,website'])->group( function () {
     //protected routes
+    Route::post('weather/getData', 'App\Http\Controllers\WeatherdataController@getData');
+    Route::post('weather/getDataV2', 'App\Http\Controllers\WeatherdataController@getAdvData');
+    Route::get('weather', 'App\Http\Controllers\WeatherdataController@index');
 });
 
 //Route::middleware(['auth:sanctum', 'abilities:website'])->group( function () {
